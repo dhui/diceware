d9f8059d678155212d8c87ca6a482b5191f050f3
Dale Hui <dhui@users.noreply.github.com>
2018-01-11 21:51:42 -0800

    Various improvements to generate_work_lookup.py     - Ensure that old gpg keys can be imported by verifying the gpg binary version     - Use logging so warnings and errors are displayed in stderr while stdout is redirected     - Support pulling in wordlists that aren't signed or encrypted     - Perform basic verification of wordlists

--- generate_word_lookup.py
+++ generate_word_lookup.py
@@ -1,9 +1,16 @@
 """
 Generates the wordlists to use.
+
 You'll need GnuPG installed to verify signatures.
 Once GnuPG is installed, run "gpg --search-keys reinhold@world.std.com" to add the public keys.
+Note: The key above is very old and needs GnuPG 1.4.19 or earlier to import and use.
+
 To regenerate the word_lookups, run: "python generate_word_lookup.py > word_lookups.py"
 """
+
+import logging
+
+from distutils.version import LooseVersion
 
 import gnupg
 import requests
@@ -13,6 +20,14 @@ WORDLIST_URLS = {
     "beale": "http://world.std.com/~reinhold/beale.wordlist.asc"
 }
 
+MAX_GPG_VERSION_STR = "1.4.19"
+MAX_GPG_VERSION = LooseVersion(MAX_GPG_VERSION_STR)
+
+EXPECTED_WORDLIST_SIZE = 7776
+
+logger = logging.getLogger("diceware")
+logging.basicConfig(format="%(levelname)s %(message)s", level=logging.WARNING)
+
 
 def convert_wordlist_row_to_python(row):
     row_s = row.split("\t")
@@ -20,16 +35,37 @@ def convert_wordlist_row_to_python(row):
 
 
 def main():
-    gpg = gnupg.GPG()
-    for wordlist_name, url in WORDLIST_URLS.iteritems():
-        signed_response = requests.get(url)
-        response = gpg.decrypt(signed_response.text)  # decrypt will verify the signature if it's not encrypted
+    # If gnupg may have issues finding your binary, so you may need to specify it e.g. gnupg.GPG("/usr/local/bin/gpg")
+    # If gnupg may also have issues finding your gnupg home directory, so that may also need to be specified.
+    gpg = gnupg.GPG("/usr/local/bin/gpg")
+    if LooseVersion(gpg.binary_version) > MAX_GPG_VERSION:
+        logger.error("gpg version (%s) is too new, it must be <= %s" % (gpg.binary_version, MAX_GPG_VERSION_STR))
+        return
 
-        if not response.valid:
-            print "Invalid signed response for wordlist: %s with url: %s" % (wordlist_name, url)
+    for wordlist_name, url in WORDLIST_URLS.iteritems():
+        response = requests.get(url)
+        data = response.text
+
+        if url.endswith(".asc"):
+            decrypted = gpg.decrypt(response.text)  # decrypt will verify the signature if it's not encrypted
+            if decrypted.valid:
+                data = decrypted.data
+            else:
+                logger.warning("Skipping wordlist! Invalid signed response for wordlist: %s with url: %s" %
+                               (wordlist_name, url))
+                continue
+
+        rows = [r for r in data.splitlines() if r]
+        word_lookup = dict(convert_wordlist_row_to_python(r) for r in rows)
+        if len(word_lookup) != EXPECTED_WORDLIST_SIZE:
+            logger.warning("Skipping wordlist %s! %s entries found instead of %s" %
+                           (wordlist_name, len(word_lookup), EXPECTED_WORDLIST_SIZE))
+            continue
+        if len(set(word_lookup.itervalues())) != EXPECTED_WORDLIST_SIZE:
+            logger.warning("Skipping wordlist %s! %s unique words found instead of %s" %
+                           (wordlist_name, len(word_lookup), EXPECTED_WORDLIST_SIZE))
             continue
 
-        word_lookup = dict(convert_wordlist_row_to_python(r) for r in response.data.splitlines() if r)
         print "%s_WORD_LOOKUP = %s" % (wordlist_name.upper(), repr(word_lookup))
 
 
